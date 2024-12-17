2289b674532f9dbdea59fda777661a81723c22e9
Prashanthi Ramesh <rvprashanthi@gmail.com>
2024-12-17 18:15:30 -0500

    Add primary font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,3 @@
-import { Lusitana } from 'next/font/google';
-
-export const lusitana = Lusitana({
-    subsets: ['latin'],
-    weight: '400'
-});
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
